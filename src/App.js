3817bfeb8a6501f2394e287bcdafc4bc43349df8
Nicolofranceschi <49746918+Nicolofranceschi@users.noreply.github.com>
2021-04-05 21:51:14 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Ciao raga</h1>
+      <h2>Questo è il mio primo componente</h2>
     </div>
   );
 }
