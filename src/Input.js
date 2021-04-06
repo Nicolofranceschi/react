c5e9cd3c51510e1660578bd895df7a96087c27cf
Nicolofranceschi <Nicolofranceschi@users.noreply.github.com>
2021-04-06 15:11:38 +0200

    reggg

--- src/Input.js
+++ src/Input.js
@@ -1,24 +1,18 @@
-
-
+import React from 'react';
 import { useForm } from 'react-hook-form';
 
+export function Input() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = data => { console.log(data); }
 
-export  function Input() {
-  const { register, handleSubmit } = useForm();
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
+            <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
+            <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
+            <input type="tel" placeholder="Mobile number" {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })} />
 
-  const onSubmit = data => {
-      console.log(data);
-  }
-
-  return (
-
-    <form onSubmit={handleSubmit(onSubmit)}>
-
-      <input type="text" placeholder="Nome" {...register("Nome", {required: true, maxLength: 80})} />
-
-      <input type="submit" />
-
-    </form>
-
-  );
+            <input type="submit" />
+        </form>
+    );
 }
